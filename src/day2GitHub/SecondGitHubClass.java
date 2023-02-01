794479c37852fadf7796d8aaad7e22b1f4f79bc4
umutcandan <umt.cndan@gmail.com>
2023-01-31 20:47:09 -0500

    some changes done on  this class to the loginFeature

--- src/day2GitHub/SecondGitHubClass.java
+++ src/day2GitHub/SecondGitHubClass.java
@@ -3,5 +3,7 @@ package day2GitHub;
 public class SecondGitHubClass {
     public static void main(String[] args) {
         System.out.println("Hello  Batch7");
+        System.out.println("2+2");
+
     }
 }
